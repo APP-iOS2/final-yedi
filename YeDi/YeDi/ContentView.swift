4f3582d110918fb5e3accf79c2804efe60f4630b
Rudy-009 <rofxnaos@gmail.com>
2023-09-24 22:48:31 +0900

    Feat: 프로젝트 생성

---
+++ YeDi/YeDi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YeDi
+//
+//  Created by 이승준 on 2023/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YeDi/YeDi/YeDiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeDiApp.swift
+//  YeDi
+//
+//  Created by 이승준 on 2023/09/22.
+//
+
+import SwiftUI
+
+@main
+struct YeDiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
