0a7f62b3ba4836f50c71f08e5971a412504d9b00
Rudy-009 <rofxnaos@gmail.com>
2023-09-25 14:30:20 +0900

    Feat: 임시 계정 생성및 빠른 로그인 기능 구현

---
+++ YeDi/YeDi/UserAuth.swift
@@ -0,0 +1,14 @@
+//
+//  UserAuth.swift
+//  YeDi
+//
+//  Created by 이승준 on 2023/09/25.
+//
+
+import SwiftUI
+import Combine
+
+class UserAuth: ObservableObject {
+    @Published var isLogged = false
+}
+
