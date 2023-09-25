87e8e8c0d915a4008f6af809ead730582763ddde
choala <pcy07111@gmail.com>
2023-09-25 18:01:53 +0900

    Feat: Create a profile view skeleton code

---
+++ YeDi/YeDi/Client/View/CMProfile/CMFollowingListView.swift
@@ -0,0 +1,21 @@
+//
+//  CMFollowingListView.swift
+//  YeDi
+//
+//  Created by 박채영 on 2023/09/25.
+//
+
+import SwiftUI
+
+struct CMFollowingListView: View {
+    var body: some View {
+        VStack {
+            Text("팔로잉한 디자이너가 없습니다.")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    CMFollowingListView()
+}
---
+++ YeDi/YeDi/Client/View/CMProfile/CMLikeListView.swift
@@ -0,0 +1,21 @@
+//
+//  CMLikeListView.swift
+//  YeDi
+//
+//  Created by 박채영 on 2023/09/25.
+//
+
+import SwiftUI
+
+struct CMLikeListView: View {
+    var body: some View {
+        VStack {
+            Text("찜한 게시물이 없습니다.")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    CMLikeListView()
+}
