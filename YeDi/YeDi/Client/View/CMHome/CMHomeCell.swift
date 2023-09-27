07fd654c5cfa24de981bc1fb78642eddfea7d071
J <134610315+Jaehui-Yu@users.noreply.github.com>
2023-09-27 15:28:51 +0900

    Feat: HomeView에 FireStore 데이터 불러오기 + CMPostViewModel을 통해 Fetch

--- YeDi/YeDi/Client/View/CMHome/CMHomeCell.swift
+++ YeDi/YeDi/Client/View/CMHome/CMHomeCell.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct CMHomeCell: View {
+    var post: Post
+    @State private var selectedImageIndex: Int = 0
+    @State private var isLiked: Bool = false
+    
     var body: some View {
         VStack {
             HStack {
@@ -15,8 +19,10 @@ struct CMHomeCell: View {
                     .resizable()
                     .frame(width: 50, height: 50)
                 VStack(alignment: .leading) {
-                    Text("디자이너 이름")
-                    Text("디저이너 근무 지점")
+                    Text(post.designerID)
+                    Text(post.location)
+                        .font(.callout)
+                        .foregroundStyle(.gray)
                 }
                 Spacer()
                 Button(action: {}, label: {
@@ -24,25 +30,38 @@ struct CMHomeCell: View {
                 })
                 .buttonStyle(.borderedProminent)
                 .tint(.black)
-
+                
             }
             .padding(.horizontal)
-            ScrollView(.horizontal) {
-                LazyHStack(content: {
-                    ForEach(1...5, id: \.self) { image in
-                        Image(systemName: "square.fill")
-                            .resizable()
-                            .frame(width: 300, height: 300)
-                            .padding()
+            
+            if post.photos.count == 1 {
+                // 이미지가 한 장인 경우
+                AsyncImage(url: post.photos[0].imageURL, placeholder: Image(systemName: "photo"))
+                    .frame(width: 360, height: 360)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(8)
+            } else {
+                // 이미지가 여러 장인 경우
+                TabView(selection: $selectedImageIndex) {
+                    ForEach(0..<post.photos.count, id: \.self) { index in
+                        AsyncImage(url: post.photos[index].imageURL, placeholder: Image(systemName: "photo"))
+                            .frame(width: 360, height: 360)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(8)
+                            .tag(index)
                     }
-                })
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .frame(height: 360)
+                .padding(.horizontal)
             }
+            
             HStack {
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Image(systemName: "heart")
+                Button(action: {isLiked.toggle()}, label: {
+                    Image(systemName: isLiked ? "heart.fill" : "heart")
                         .resizable()
                         .frame(width: 25, height: 25)
-                        .foregroundColor(.black)
+                        .foregroundColor(isLiked ? .red : .black)
                 })
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
                     HStack {
@@ -60,5 +79,5 @@ struct CMHomeCell: View {
 }
 
 #Preview {
-    CMHomeCell()
+    CMHomeCell(post: Post(id: "1", designerID: "디자이너 이름", location: "디자이너 근무 지점", title: "게시물 제목", description: "게시물 설명", photos: [Photo(id: "1", imageURL: "https://example.com/image1.jpg"), Photo(id: "2", imageURL: "https://example.com/image2.jpg")], comments: 0, timestamp: "타임스탬프"))
 }
