5f8aaa0c3006da0b947d1a0ee4120348bb89216d
Rudy-009 <rofxnaos@gmail.com>
2023-09-27 11:15:23 +0900

    Feat: CommonBubble에 각 버블 자료형에 따른 서로 다른 init 함수 추가

--- YeDi/YeDi/Shared/ViewModel/ChattingViewModel.swift
+++ YeDi/YeDi/Shared/ViewModel/ChattingViewModel.swift
@@ -30,59 +30,33 @@ class TempChatbubbleStore: ObservableObject {
     }
     
     func fetchChattingBubble(chatRomsId: String) {
-            self.ref.child("chatRooms").child(chatRomsId).child("chatBubbles").observe(.value) { snapshot  in
-                guard let chatData = snapshot.value as? [String : Any] else {
-                      print("Error reading data")
-                      return
-                }
-                var bubbles: [CommonBubble] = []
-                
-                for (key, value) in chatData {
-                    do {
-                        var value = value as! [String : Any]
-                        value["id"] = key
-                        
-                        let jsonData = try JSONSerialization.data(withJSONObject: value)
-                    
-                        let bubble = try JSONDecoder().decode(CommonBubble.self, from: jsonData)
-                        bubbles.append(bubble)
-                    } catch {
-                        print("Error decoding bubble data")
-                    }
-                }
-                bubbles.sort(by: {$0.date < $1.date})
-                self.chattings = bubbles
+        self.ref.child("chatRooms").child(chatRomsId).child("chatBubbles").observe(.value) { snapshot  in
+            guard let chatData = snapshot.value as? [String : Any] else {
+                print("Error reading data")
+                return
             }
+            var bubbles: [CommonBubble] = []
+            
+            for (key, value) in chatData {
+                do {
+                    var value = value as! [String : Any]
+                    value["id"] = key
+                    
+                    let jsonData = try JSONSerialization.data(withJSONObject: value)
+                    
+                    let bubble = try JSONDecoder().decode(CommonBubble.self, from: jsonData)
+                    bubbles.append(bubble)
+                } catch {
+                    print("Error decoding bubble data")
+                }
+            }
+            bubbles.sort(by: {$0.date < $1.date})
+            self.chattings = bubbles
         }
-    
-    func loadAndUpdateChatting() {
-//
-//        let chatbubbleRef = ref
-//            .child("chatRooms")
-//            .child(chatRoomID)
-//            .child("chatBubbles") //채팅방 경로 (Firebae Realtime Database)
-//            .queryOrdered(byChild: "date") //date 프로퍼티로 정렬
-//
-//        chatbubbleRef.observe(.value) { snapshot in
-//
-//            guard let value = snapshot.value as? [String: [String: Any]] else {
-//                print("Error: Unable to parse snapshot data")
-//                return
-//            }
-//
-//            var tempBubbles = self.decodeToBubble(snapshotValue: value)
-//
-//            DispatchQueue.main.async { // UI 업데이트는 메인 스레드에서 실행되어야 합니다.
-//                self.chats = tempBubbles
-//                //print("==========chats")
-//                //print(self.chats)
-//            }
-//
-//        }
     }
     
     func sendBoardBubble(content: String, imagePath: String, sender: String) {
-        let bubble = BoardBubble(
+        let bubble = CommonBubble(
             content: content,
             imagePath: imagePath,
             date: "\(Date())",
@@ -93,14 +67,15 @@ class TempChatbubbleStore: ObservableObject {
                 "content": bubble.content,
                 "imagePath": bubble.imagePath,
                 "date": bubble.date,
-                "messageType": "boardBubble",
+                "messageType": bubble.messageType.rawValue,
                 "sender": bubble.sender
             ])
-        loadAndUpdateChatting()
+        
+        fetchChattingBubble(chatRomsId: chatRoomID)
     }
     
     func sendTextBubble(content: String, sender: String) {
-        let bubble = TextBubble(
+        let bubble = CommonBubble(
             content: content,
             date: "\(Date())",
             sender: sender
@@ -110,14 +85,15 @@ class TempChatbubbleStore: ObservableObject {
             .setValue([
                 "content": bubble.content,
                 "date": bubble.date,
-                "messageType": "boardBubble",
+                "messageType": bubble.messageType.rawValue,
                 "sender": bubble.sender
             ])
-        loadAndUpdateChatting()
+        
+        fetchChattingBubble(chatRomsId: chatRoomID)
     }
     
     func sendImageBubble(imagePath: String, sender: String) {
-        let bubble = ImageBubble(
+        let bubble = CommonBubble(
             imagePath: imagePath,
             date: "\(Date())",
             sender: sender
@@ -127,46 +103,14 @@ class TempChatbubbleStore: ObservableObject {
             .setValue([
                 "imagePath": bubble.imagePath,
                 "date": bubble.date,
-                "messageType": "boardBubble",
+                "messageType": bubble.messageType.rawValue,
                 "sender": bubble.sender
             ])
-        loadAndUpdateChatting()
+        
+        fetchChattingBubble(chatRomsId: chatRoomID)
     }
     
-    func decodeToBubble(snapshotValue: [String: [String: Any]]) -> [Any] {
-        return snapshotValue.compactMap { key, value in
-            guard
-                let messageType = value["messageType"] as? String,
-                let date = value["date"] as? String,
-                let sender = value["sender"] as? String
-            else {
-                return nil // 필수 필드가 없는 경우는 무시
-            }
-            print("MT is : \(messageType)")
-            switch messageType {
-            case MessageType.boardBubble.rawValue:
-                guard
-                    let content = value["content"] as? String,
-                    let imagePath = value["ImagePath"] as? String
-                else {
-                    return nil
-                }
-                return BoardBubble(id: key, content: content, imagePath: imagePath, date: date, sender: sender)
-            case MessageType.textBubble.rawValue:
-                guard let content = value["content"] as? String else {
-                    return nil
-                }
-                return TextBubble(id: key, content: content, date: date, sender: sender)
-            case MessageType.imageBubble.rawValue:
-                guard let imagePath = value["ImagePath"] as? String else {
-                    return nil
-                }
-                return ImageBubble(id: key, imagePath: imagePath, date: date, sender: sender)
-            default:
-                return nil // 알 수 없는 messageType인 경우 무시
-            }
-        }
-    }
+    
     
 }
 
