bdd50c4288045012b74e44edb3d8eaaf496f53bb
Rudy-009 <rofxnaos@gmail.com>
2023-10-04 14:35:22 +0900

    Dev 백업본

--- YeDi/YeDi/Client/View/ClientMainTabView.swift
+++ YeDi/YeDi/Client/View/ClientMainTabView.swift
@@ -11,48 +11,57 @@ struct ClientMainTabView: View {
     @State private var selectedIndex = 0
     
     var body: some View {
-        NavigationStack {
-            TabView(selection: $selectedIndex) {
+        TabView(selection: $selectedIndex) {
+            NavigationStack {
                 CMHomeView()
-                .onTapGesture {
-                    self.selectedIndex = 0
-                }
-                .tabItem {
-                    Label("홈", systemImage: "house")
-                }.tag(0)
-                
-                CMFilterView()
-                .onTapGesture {
-                    self.selectedIndex = 1
-                }
-                .tabItem {
-                    Label("필터", systemImage: "")
-                }.tag(1)
-                
-                CMReservationView()
-                .onTapGesture {
-                    self.selectedIndex = 2
-                }
-                .tabItem {
-                    Label("예약", systemImage: "")
-                }.tag(2)
-                
-                CMMainChattingView()
-                .onTapGesture {
-                    self.selectedIndex = 3
-                }
-                .tabItem {
-                    Label("채팅", systemImage: "")
-                }.tag(3)
-                
-                CMProfileView()
-                .onTapGesture {
-                    self.selectedIndex = 4
-                }
-                .tabItem {
-                    Label("프로필", systemImage: "")
-                }.tag(4)
             }
+            .onTapGesture {
+                self.selectedIndex = 0
+            }
+            .tabItem {
+                Label("홈", systemImage: "house")
+            }.tag(0)
+            
+            NavigationStack {
+                CMFilterView()
+            }
+            .onTapGesture {
+                self.selectedIndex = 1
+            }
+            .tabItem {
+                Label("필터", systemImage: "")
+            }.tag(1)
+            
+            NavigationStack {
+                CMReservationView()
+            }
+            .onTapGesture {
+                self.selectedIndex = 2
+            }
+            .tabItem {
+                Label("예약", systemImage: "")
+            }.tag(2)
+            
+            NavigationStack {
+                CMMainChattingView()
+            }
+            .onTapGesture {
+                self.selectedIndex = 3
+            }
+            .tabItem {
+                Label("채팅", systemImage: "")
+            }.tag(3)
+            
+            NavigationStack {
+                CMProfileView()
+            }
+            .onTapGesture {
+                self.selectedIndex = 4
+            }
+            .tabItem {
+                Label("프로필", systemImage: "")
+            }.tag(4)
+            
         }
     }
 }
--- YeDi/YeDi/Designer/View/DMChattingView/DMMainChattingView.swift
+++ YeDi/YeDi/Designer/View/DMChattingView/DMMainChattingView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DMMainChattingView: View {
     var body: some View {
-        ChattingListRoomView()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
