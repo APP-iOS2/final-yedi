d8c8b124f8522356eb320e935ce38f925f0914a9
songseongwook <softfrog@kakao.com>
2023-10-11 18:39:05 +0900

    rename: review, reservation view 복구 /파일 삭제

---
+++ YeDi/YeDi/Designer/View/DMReview/DMReviewDetailView.swift
@@ -0,0 +1,41 @@
+//
+//  DMReviewDetailView.swift
+//  YeDi
+//
+//  Created by 송성욱 on 10/6/23.
+//
+
+import SwiftUI
+import FirebaseFirestore
+
+
+struct DMReviewDetailView: View {
+    
+    var review: Review
+    
+    var body: some View {
+        List {
+            Section("고객명") {
+                Text("\(review.reviewer)")
+            }
+            Section("작성일") {
+                Text("\(review.date)")
+            }
+            Section("평점") {
+                Text("\(review.designerScore)")
+            }
+            Section("디자이너 스타일 평가") {
+                Text("\(review.style)")
+            }
+            Section("리뷰 내용") {
+                Text("\(review.content)")
+            }
+        }
+        .listStyle(.plain)
+        .navigationTitle("상세리뷰")
+    }
+}
+
+//#Preview {
+//    DMReviewDetailView()
+//}
