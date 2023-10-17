ca11675ee02392e8b4d147d59439f64dc7196b8f
Yunji Kim <yunji.kmm@gmail.com>
2023-10-18 05:15:13 +0900

    Fix: modify mainColor dark appearance

---
+++ YeDi/YeDi/Shared/ViewModifier/TextFieldModifier.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldModifier.swift
+//  YeDi
+//
+//  Created by yunjikim on 2023/10/18.
+//
+
+import SwiftUI
+
+struct TextFieldModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextFieldModifier()
+}
