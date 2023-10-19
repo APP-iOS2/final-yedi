046307329838e870427efc3313d942cafcf32497
Chan Ho Park <park_ch@kakao.com>
2023-10-20 02:51:28 +0900

    Feat: 디자이너 마이페이지 샵 정보 패치

--- YeDi/YeDi/Designer/View/DMProfileView/ProfileEditView/DMProfileView.swift
+++ YeDi/YeDi/Designer/View/DMProfileView/ProfileEditView/DMProfileView.swift
@@ -52,21 +52,26 @@ struct DMProfileView: View {
                 }
                 .padding([.leading], 20)  // HStack의 전체 왼쪽 패딩을 조절
                 
+                // 샵 정보 섹션
                 VStack {
                     VStack(alignment: .leading) {
                         Text(profileVM.shop.shopName)  // 샵 이름
                             .font(.title3)
                             .fontWeight(.semibold)
+                        
                         Text(profileVM.shop.headAddress)  // 샵 위치
                             .foregroundStyle(.gray)
+                        
                         Divider()
-                        Text("휴무일: \(profileVM.shop.closedDays.joined(separator: ", "))월 요일")  // 휴무일 정보
+                        
+                        Text("휴무일: \(profileVM.shop.closedDays.joined(separator: ", "))")  // 휴무일 정보
                             .foregroundStyle(.gray)
                     }
                     .padding()
                 }
                 .background(.gray.opacity(0.2))
                 .padding()
+
                 
                 // 프로필 편집으로 이동하는 버튼
                 NavigationLink {
