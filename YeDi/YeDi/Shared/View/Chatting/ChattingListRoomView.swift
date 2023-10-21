079507daf36b1c78fc3d13a3398b5da4aeb3b3c0
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2023-10-22 00:12:48 +0900

    Fix: 채팅 리스트 최근 메세지 날짜 표출형식 변경

--- YeDi/YeDi/Shared/View/Chatting/ChattingListRoomView.swift
+++ YeDi/YeDi/Shared/View/Chatting/ChattingListRoomView.swift
@@ -84,11 +84,31 @@ struct ChattingListRoomView: View {
     }
     
     private func changetoDateFormat(_ messageDate: String) -> String {
-        let instance = SingleTonDateFormatter.sharedDateFommatter
-        let date = instance.changeDateString(transition: "MM/dd HH:mm", from: messageDate)
-        return date
+        let dateFomatter = SingleTonDateFormatter.sharedDateFommatter.firebaseDateFormat()
+        let date = dateFomatter.date(from: messageDate) ?? Date()
+        let calendar = Calendar.current
+        
+        if calendar.isDateInToday(date) {
+            dateFomatter.dateFormat = "HH:mm"
+            return dateFomatter.string(from: date)
+        } else if calendar.isDateInYesterday(date) {
+            return "어제"
+        } else {
+            let currentYear = calendar.component(.year, from: Date())
+            let messageYear = calendar.component(.year, from: date)
+                // 올해 년도의 메세지인 경우 월/일 반환
+            if currentYear == messageYear {
+                dateFomatter.dateFormat = "MM/dd"
+                return dateFomatter.string(from: date)
+            } else {
+                // 그 외 올해가 아닌 데이터 날짜는 년.월, 일 형식의 String을 반환
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy.MM.dd"
+                return formatter.string(from: date)
+            }
+        }
     }
-    
+
 }
 
 #Preview {
