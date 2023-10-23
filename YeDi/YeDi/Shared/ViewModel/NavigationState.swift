499275604ef0155b2ea255db96a2f19e5f543ba1
SonYoonho <yoonhoson@naver.com>
2023-10-23 21:38:38 +0900

    Feat: 예약기능 구현

---
+++ YeDi/YeDi/Shared/ViewModel/NavigationState.swift
@@ -0,0 +1,12 @@
+//
+//  NavigationState.swift
+//  YeDi
+//
+//  Created by SONYOONHO on 2023/10/23.
+//
+
+import Foundation
+
+final class NavigationState: ObservableObject {
+    @Published var path: [Int] = []
+}
