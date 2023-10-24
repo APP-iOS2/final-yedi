e5fa0955ebfdf33166d59b408336612a248e7955
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2023-10-24 10:00:39 +0900

    Design: 이미지, 게시글 버블 이미지 표출 캐싱 이미지 처리 구조체로 변경

--- YeDi/YeDi/Shared/View/Chatting/ImageBubbleCell.swift
+++ YeDi/YeDi/Shared/View/Chatting/ImageBubbleCell.swift
@@ -11,13 +11,8 @@ struct ImageBubbleCell: View {
     var imagePath: String
     
     var body: some View {
-        AsyncImage(url: URL(string: imagePath)) { image in
-            image
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(maxWidth: 220)
-        } placeholder: {
-            ProgressView()
-        }
+        DMAsyncImage(url: imagePath)
+            .aspectRatio(contentMode: .fit)
+            .frame(maxWidth: 220)
     }
 }
